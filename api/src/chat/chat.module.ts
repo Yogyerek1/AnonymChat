eac4bdbd2c3b786b56800a885cdbe3e0763b071b
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-13 21:28:23 +0100

    config(AC-03): configurate modules

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from '../entities/message.entity';
+import { Chat } from '../entities/chat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chat, Message])],
   controllers: [ChatController],
   providers: [ChatService],
 })
