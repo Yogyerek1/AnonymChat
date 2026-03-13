bbf27093db1aaab556f7b9f50f8175f14770d90d
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-13 21:24:32 +0100

    feat(AC-03): add Chat and Mesage entities

---
+++ api/src/entities/chat.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Message } from './message.entity';
+
+@Entity('chats')
+export class Chat {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Message, (message) => message.chat)
+  messages: Message[];
+}
