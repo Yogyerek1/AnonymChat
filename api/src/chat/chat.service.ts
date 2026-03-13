77322dcbe8209e69cbe7528ddc128aae15998e2c
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-13 21:31:34 +0100

    feat(AC-03): inject Chat and Message repositories

--- api/src/chat/chat.service.ts
+++ api/src/chat/chat.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Chat } from '../entities/chat.entity';
+import { Message } from '../entities/message.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectRepository(Chat) private chatRepository: Repository<Chat>,
+    @InjectRepository(Message) private messageRepository: Repository<Message>,
+  ) {}
+}
