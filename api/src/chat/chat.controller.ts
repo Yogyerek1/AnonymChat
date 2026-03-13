7b3353f849257dc7db399aaa4a43c89c881643b1
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-13 21:59:46 +0100

    feat(AC-03): add createChat and joinChat endpoints to ChatController

--- api/src/chat/chat.controller.ts
+++ api/src/chat/chat.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { CreateChatDto } from './dto/createChat.dto';
+import { ChatService } from './chat.service';
+import { JoinChatDto } from './dto/joinChat.dto';
 
 @Controller('chats')
-export class ChatController {}
+export class ChatController {
+  constructor(private chatService: ChatService) {}
+
+  @Post()
+  createChat(@Body() body: CreateChatDto) {
+    return this.chatService.createChat(body);
+  }
+
+  @Post('join')
+  joinChat(@Body() body: JoinChatDto) {
+    return this.chatService.joinChat(body);
+  }
+}
